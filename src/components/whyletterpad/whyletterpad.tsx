1f93b9e8d451546e3af53f61584242d4582fe836
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2021-12-24 17:57:34 +0100

    change assets

--- src/components/whyletterpad/whyletterpad.tsx
+++ src/components/whyletterpad/whyletterpad.tsx
@@ -4,7 +4,7 @@ const WhyLetterpad = () => {
   return (
     <div className='mt-20 w-full md:w-full'>
       <h3 className='px-4'>Why Letterpad ?</h3>
-      <div className='flex flex-wrap p-4 mt-4'>
+      <div className='flex flex-wrap mt-4 p-4'>
         <div className='w-full md:pr-6 md:w-1/2'>
           Letterpad has no distractions, no sidebars, plugins or widgets, with
           nothing to setup or customize, it only has the essentials for a pure
@@ -17,11 +17,11 @@ const WhyLetterpad = () => {
           tablet devices. Letterpad gives you the choice of writing in Markdown
           or in Rich Text.
         </div>
-        <div className='w-full lg:w-1/2'>
+        <div className='flex justify-center mt-14 w-full lg:mt-0 lg:w-1/2'>
           <Image
             src='/images/client.png'
-            width='450px'
-            height='300px'
+            width='460px'
+            height='340px'
             alt='Letterpad Client'
             className='shadow-lg'
           />
