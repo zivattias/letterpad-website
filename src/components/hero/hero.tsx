c9c00274a01082891485b6fd7ef69b7502749ce8
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-03-20 01:35:29 +0530

    replace hero image

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -7,12 +7,12 @@ const Hero = () => {
     <section className='bg-white'>
       <div className='container flex flex-col items-center mx-auto my-12 md:flex-row md:my-24'>
         {/* <!--Left Col--> */}
-        <div className='flex flex-col justify-center items-start px-4 pt-12 pb-24 w-full lg:w-1/2'>
+        <div className='flex flex-col items-start justify-center pb-24 pt-12 px-4 w-full lg:w-1/2'>
           <p className='tracking-loose uppercase'></p>
-          <h1 className='my-4 text-3xl font-bold'>
+          <h1 className='font-bold my-4 text-3xl'>
             Letterpad is a blog publishing platform.
           </h1>
-          <p className='mb-4 leading-normal'>
+          <p className='leading-normal mb-4'>
             Letterpad has a minimal footprint in terms of features and are best
             suited for personal blogs.
           </p>
@@ -36,9 +36,9 @@ const Hero = () => {
           </p>
         </div>
         {/* <!--Right Col--> */}
-        <div className='w-full text-center lg:py-6 lg:w-1/2'>
+        <div className='text-center w-full lg:py-6 lg:w-1/2'>
           <Image
-            src='/images/hero.jpg'
+            src='/images/demo.png'
             alt='Demo Image'
             width={800}
             height={520}
