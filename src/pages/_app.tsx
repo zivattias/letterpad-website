521ea57a6aa7d33f4f47b465bc9f6e07afae1545
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-05-26 00:15:07 +0200

    add: ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,35 @@
 import { AppProps } from "next/app";
+import Script from "next/script";
 
 import "@/styles/globals.css";
 
 import Layout from "@/components/layout/Layout";
 
+const GA = "G-6BXBVG7328";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA}`}
+      />
+
+      <Script id='ga-init' strategy='lazyOnload'>
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
