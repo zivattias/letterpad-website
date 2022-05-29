1dff51ab823483467eaaf1c0f22ff8217f4436ae
Abhishek Saha <1502352+abhisaha1@users.noreply.github.com>
2022-05-29 18:32:36 +0200

    chore: spacing

--- src/components/post-image/post-image.tsx
+++ src/components/post-image/post-image.tsx
@@ -4,7 +4,7 @@ import Screenshot from "./post.png";
 
 export const PostImage = () => {
   return (
-    <div className='mx-auto md:mt-24 md:w-4/5'>
+    <div className='flex justify-center mx-auto md:mt-24 md:w-4/5'>
       <Image
         src={Screenshot}
         alt='Post'
