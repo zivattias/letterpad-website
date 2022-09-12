fab45d5cbe0f2881e28969c8d42de58757c3ec2c
Abhishek Saha <abhishek.saha@hometogo.com>
2022-09-12 10:00:01 +0200

    chore: add better graphic

--- src/components/post-image/post-image.tsx
+++ src/components/post-image/post-image.tsx
@@ -1,16 +1,11 @@
 import Image from "next/image";
 
-import Screenshot from "./post.png";
+import Screenshot from "./post2.png";
 
 export const PostImage = () => {
   return (
-    <div className='flex justify-center mx-auto md:mt-24 md:w-4/5'>
-      <Image
-        src={Screenshot}
-        alt='Post'
-        // layout='responsive'
-        // objectFit='contain'
-      />
+    <div className='flex justify-center mx-auto mb-20 md:mt-24 md:w-4/5'>
+      <Image src={Screenshot} alt='Post' />
     </div>
   );
 };
