a5f4ae8a995395e5831871bc7049b6eea1f7327f
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-09 01:19:24 +0530

    fix: casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,38 @@
-import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-import 'aos/dist/aos.css';
-import './css/style.css';
+import "aos/dist/aos.css";
+import "./css/style.css";
 
-import AOS from 'aos';
+import AOS from "aos";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import { useTracking } from './utils/usetracking';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { useTracking } from "./utils/useTracking";
 
 function App() {
-
   const location = useLocation();
   useTracking();
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 500,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
       </Routes>
     </>
   );
