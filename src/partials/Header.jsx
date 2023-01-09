903cf47f618b5238be00bd7c5a155df24b1931ce
Abhishek Saha <abhishek.saha@hometogo.com>
2023-01-09 14:16:20 +0530

    fix: cta button in navbar

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../images/logo.png";
 
 function Header() {
   return (
-    <header className="absolute w-full z-30">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="flex items-center justify-between h-16 md:h-20">
+    <header className='absolute w-full z-30'>
+      <div className='max-w-6xl mx-auto px-4 sm:px-6'>
+        <div className='flex items-center justify-between h-16 md:h-20'>
           {/* Site branding */}
-          <div className="shrink-0 mr-4">
+          <div className='shrink-0 mr-4'>
             {/* Logo */}
-            <Link className="block" to="/" aria-label="Letterpad">
-              <img className="mx-auto md:max-w-none" width={40} src={Logo} alt="Letterpad" />
+            <Link className='block' to='/' aria-label='Letterpad'>
+              <img
+                className='mx-auto md:max-w-none'
+                width={40}
+                src={Logo}
+                alt='Letterpad'
+              />
             </Link>
           </div>
 
           {/* Desktop navigation */}
-          <nav className="flex grow">
+          <nav className='flex grow'>
             {/* Desktop sign in links */}
-            <ul className="flex grow justify-end flex-wrap items-center">
+            <ul className='flex grow justify-end flex-wrap items-center'>
               <li>
-                <Link className="font-medium text-gray-400 hover:text-blue-500 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out" to="/admin/login" onClick={() => {
-                  track({
-                    eventAction: EventAction.Click,
-                    eventCategory: "login",
-                    eventLabel: `header`,
-                  });
-                }}>Sign in</Link>
+                <Link
+                  className='font-medium text-gray-400 hover:text-blue-500 px-3 lg:px-5 py-2 flex items-center transition duration-150 ease-in-out'
+                  to='/admin/login'
+                  onClick={() => {
+                    track({
+                      eventAction: EventAction.Click,
+                      eventCategory: "login",
+                      eventLabel: `navbar`,
+                    });
+                  }}
+                >
+                  Sign in
+                </Link>
               </li>
-              <li className="ml-3">
-                <Link className="btn-sm text-white bg-gradient-to-t from-blue-600 to-blue-400 hover:to-blue-500 w-full shadow-lg group" to="#0">
-                  Get Started <span className="tracking-normal text-blue-200 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1">-&gt;</span>
+              <li className='ml-3'>
+                <Link
+                  className='btn-sm text-white bg-gradient-to-t from-blue-600 to-blue-400 hover:to-blue-500 w-full shadow-lg group'
+                  to='/admin/register'
+                  onClick={() => {
+                    track({
+                      eventAction: EventAction.Click,
+                      eventCategory: "register",
+                      eventLabel: `navbar`,
+                    });
+                  }}
+                >
+                  Get Started{" "}
+                  <span className='tracking-normal text-blue-200 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1'>
+                    -&gt;
+                  </span>
                 </Link>
               </li>
             </ul>
